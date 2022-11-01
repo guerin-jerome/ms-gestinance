6e82490cbb5a4854b99573aba4a30af9e91298fd
Jérôme Guèrin <guerin_jerome@icloud.com>
2022-11-01 12:00:02 +0100

    feat(starter): initial commit

---
+++ src/main/java/com/example/msgestinance/MsGestinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgestinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msgestinance/MsGestinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgestinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
